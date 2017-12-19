53c636bcd38ef00ee07451ff4b0b3c29856f9bdb
Robert Lowry <bobertlo@gmail.com>
2017-12-18 22:31:35 -0600

    ... back to hard

--- grid-models.py
+++ grid-models.py
@@ -101,7 +101,7 @@ for l,mi,pi,pgi in models:
 #print("Running VotingClassifier...")
 #models = [(label,clf()) for (label,clf) in models]
 
-vc = VotingClassifier(estimators, voting='soft')
+vc = VotingClassifier(estimators, voting='hard')
 vc.fit(x_train, y_train)
 print(vc.score(x_train,y_train))
 p = vc.predict(x_test)
